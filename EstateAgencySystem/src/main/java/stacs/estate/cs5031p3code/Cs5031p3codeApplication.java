a70c20b4e9b133fa402c48205abeca9710473ebf
liaomu <2376817023@qq.com>
2023-03-23 14:53:46 +0000

    Init back-end

---
+++ EstateAgencySystem/src/main/java/stacs/estate/cs5031p3code/Cs5031p3codeApplication.java
@@ -0,0 +1,15 @@
+package stacs.estate.cs5031p3code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 220032952
+ * @version 0.0.1
+ */
+@SpringBootApplication
+public class Cs5031p3codeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Cs5031p3codeApplication.class, args);
+    }
+}
